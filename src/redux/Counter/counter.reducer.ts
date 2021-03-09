a95f4b7414ef06e579d1ac612c07a7ef52ee52ef
Austin Wright <austin.wright@netspi.com>
2021-03-09 09:25:22 -0700

    Working on setting up Redux. It's broken right now.

---
+++ src/redux/Counter/counter.reducer.ts
@@ -0,0 +1,11 @@
+import { INCREMENT, DECREMENT } from './counter.types';
+
+const INITIAL_STATE: {count: number} = {
+    count: 0,
+};
+
+const reducer = (state = INITIAL_STATE, action: {type: string}) => {
+    switch (action.type) {
+
+    }
+}
---
+++ src/redux/store.ts
@@ -0,0 +1,6 @@
+import { createStore } from 'redux';
+import rootReducer from './rootReducer';
+
+export const rootReducer = createStore({
+
+})
